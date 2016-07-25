5cf10d837741fa78054ffaf6e60cd8878a1e30b3
Tom Louden <t.m.louden@warwick.ac.uk>
2016-07-25 16:43:48 -0700

    fixed memory leak...?

--- c_src/main.c
+++ c_src/main.c
@@ -69,6 +69,13 @@ double *lightcurve(int n_layers, int n_points, double *t, double tc, double per,
         phi0 = tan(coords[1]/coords[2]);
         planet = map_model(planet,n_layers,xi,T_n,delta_T,lambda0,phi0,u1,u2);
 
+        int n_segments = pow(n_layers,2);
+        for (int i = 0; i < n_segments; ++i) {
+          free(planet[i]);
+          // each i-th pointer is now pointing to dynamic array (size 10) of actual int values
+        }
+        free(planet);
+
         output[n] = 1.0;/*
 
         p_bright = 0.0;
@@ -87,12 +94,6 @@ double *lightcurve(int n_layers, int n_points, double *t, double tc, double per,
     */
     }
 
-    int n_segments = pow(n_layers,2);
-    for (int i = 0; i < n_segments; ++i) {
-      free(planet[i]);
-      // each i-th pointer is now pointing to dynamic array (size 10) of actual int values
-    }
-    free(planet);
     free(coords);
     free(transit_coords);
 
