18e2b010a6e994a014d7152e4ed17ef689ed12ed
Tom Louden <t.m.louden@warwick.ac.uk>
2016-07-25 17:03:49 -0700

    fixed memory leak...?

--- c_src/main.c
+++ c_src/main.c
@@ -67,9 +67,10 @@ double *lightcurve(int n_layers, int n_points, double *t, double tc, double per,
         }
 
         phi0 = tan(coords[1]/coords[2]);
-        map_model(planet,n_layers,xi,T_n,delta_T,lambda0,phi0,u1,u2);
 
         output[n] = 1.0;/*
+
+        map_model(planet,n_layers,xi,T_n,delta_T,lambda0,phi0,u1,u2);
 
         p_bright = 0.0;
         for (j = 0; j < pow(n_layers,2); j++) {
