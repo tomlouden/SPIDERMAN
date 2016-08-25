1e9d4976a38ee82b61600e5c9bb104f829fc2a7e
Tom Louden <t.m.louden@warwick.ac.uk>
2016-08-25 18:32:47 +0100

    version 0.3.0 release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import numpy as np
 _web = Extension("spiderman._web",["c_src/_web.c","c_src/heron.c","c_src/segment.c","c_src/areas.c","c_src/intersection.c","c_src/generate.c","c_src/blocked.c","c_src/util.c","c_src/pyutil.c","c_src/web.c","c_src/orthographic.c","c_src/ephemeris.c","c_src/blackbody.c","c_src/brightness_maps.c"])
 
 setup(	name='spiderman-package', 
-	version="0.2.2",
+	version="0.3.0",
 	author='Tom Louden',
 	author_email = 't.m.louden@warwick.ac.uk',
 	url = 'https://github.com/tomlouden/spiderman',
