e85c7b2fe57001481a5623498f826eba7a904a0b
Tom <t.m.louden@warwick.ac.uk>
2017-06-22 15:54:15 +0100

    added response function options

--- spiderman/read_model.py
+++ spiderman/read_model.py
@@ -1,3 +1,33 @@
 import spiderman as sp
 import numpy as np
 
+def open_PT_profile(fname,layer):
+	layers = []
+	for line in open(fname):
+		try:
+			layers += [int(line.strip('\n').split(',')[0])]
+		except:
+			''
+	layers = np.array(layers)
+	nlayers = np.max(layers)
+
+#	print(nlayers)
+	longitude = []
+	latitude = []
+	T = []
+
+
+	for line in open(fname):
+		try:
+			this_layer = int(line.strip('\n').split(',')[0])
+			if this_layer == layer:
+				this_grid = line.strip('\n').split(',')
+				longitude += [float(this_grid[1])]
+				latitude += [float(this_grid[2])]
+				T += [float(this_grid[4])]
+		except:
+			''
+	T = np.array(T)
+	longitude = np.array(longitude)
+	latitude = np.array(latitude)
+	return(longitude, latitude, T)
